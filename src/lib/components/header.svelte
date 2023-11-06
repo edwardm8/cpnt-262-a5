c4841f4332fca7f0bd441796c4b40d5e77dfdbe6
edwardm8 <edwardm825@gmail.com>
2023-11-06 08:43:01 -0700

    added tailwind and header

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,17 @@
+<script>
+  const navItems = [
+    {name: 'Home', href:'/'},
+    {name: 'About', href: '/about'}
+];
+</script>
+
+<header class="bg-blue-800 text-white flex justify-between p-4">
+  <div>Card Components</div>
+  <nav>
+    <ul class="flex justify-around gap-4">
+      {#each navItems as {name, href}}
+      <li><a {href}>{name}</a></li>
+      {/each}
+    </ul>
+  </nav>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "../app.css"
+  import Navbar from '../lib/components/header.svelte'
+</script>
+
+<Navbar/>
+
+<slot></slot>
