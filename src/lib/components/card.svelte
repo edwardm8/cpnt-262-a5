13296b95f5012f3a22fc756b64f7c24ce2e1e2e6
edwardm8 <edwardm825@gmail.com>
2023-11-07 08:13:12 -0700

    added multiple card

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -2,10 +2,13 @@
   import {cardInfo} from '../data.js'
 </script>
 
-
-<a href= {cardInfo.web} class="flex flex-col justify-center mx-auto text-center max-w-xl p-4 bg-blue-400
-border-2">
-  <img src= {cardInfo.img} alt="hand holding vanilla ice cream cone">
-  <h2 class="text-xl p-4">{cardInfo.title}</h2>
-  <p class="text-l">{cardInfo.description}</p>
-</a>
+<section class="flex flex-col mx-auto gap-4 p-4">
+{#each cardInfo as {web,img,title,description,alt}}
+  <a href= {web} class="flex flex-col justify-center mx-auto text-center max-w-xl p-4 bg-blue-400
+  border-2">
+    <img src= {img} alt= {alt}>
+    <h2 class="text-xl p-4">{title}</h2>
+    <p class="text-l">{description}</p>
+  </a>
+{/each}
+</section>
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,8 +1,23 @@
-
-
-export const cardInfo =
-  { title: "Permanant Frozen Ice cream",
+export const cardInfo = [
+  {
+    title: "Permanant Frozen Ice cream",
     description: "Ice cream that does not melt and stays cold the whole time. Even in scorching heat the ice cream stays intact allowing slow eaters to enojoy their ice cream. How is it done?",
     img: "/images/ice-cream.jpg",
-    web: "https://handletheheat.com/how-to-make-ice-cream/" 
+    web: "https://handletheheat.com/how-to-make-ice-cream/",
+    alt: "vanilla ice cream cone being held up"
+  },
+  {
+    title: "Flaming Hot Pizza ",
+    description: "The spiciest pizza you have ever eaten. Made with the spiciest chillies known to man.How is it done?",
+    img: "/images/pizza.jpg",
+    web: "https://www.foodandwine.com/recipes/classic-cheese-pizza",
+    alt:"freshy paked neopoltian cheese pizza"
+  },
+  {
+    title: "Delicous Pasta ",
+    description: "A easy and quick pasta recipe that you can make. Modify the dish by adding your own toppings or meats. Base version is vegitarian",
+    img: "/images/pasta.jpeg",
+    web: "https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689",
+    alt:"image of pasta close up with herbs"
   }
+];
